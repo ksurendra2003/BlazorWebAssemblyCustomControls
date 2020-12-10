244dbb8a050ed29b22ee746028bf3310bc28e4a8
surendra_babu1 <surendra_babu1@dell.com>
2020-12-10 06:57:08 +0530

    Initial Commit

---
+++ BlazorCustomControls/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCustomControls.Pages
+{
+    public partial class Index
+    {
+        protected List<string> Cources { get; set; }
+
+        public string SelectedCource { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            Cources = new List<string>
+            {
+                    "HTML",
+                    "CSS",
+                    "JavaScript",
+                    "C#"
+            };
+        }
+
+        public void SaveCource()
+        {
+            Console.WriteLine(SelectedCource);
+        }
+    }
+}
---
+++ BlazorWebAssemblyCustomControls/DropDownList.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyCustomControls
+{
+    public partial class DropDownList
+    {
+        protected string DropDownOpenCss { get; set; } = "dropdown";
+        protected string DropDownExpand { get; set; } = "false";
+
+        protected string SelectedText { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback<string> SelectedValueChanged { get; set; }
+
+        [Parameter]
+        public string SelectedValue { get; set; } = string.Empty;
+
+        [Parameter]
+        public List<string> DataSource { get; set; }
+
+        public void ShowDropDown()
+        {
+            ShowAndClose();
+        }
+
+        public Task OnItemSelect(string item)
+        {
+            SelectedText = item;
+            SelectedValue = item;
+
+            ShowAndClose();
+
+            return SelectedValueChanged.InvokeAsync(SelectedValue);
+        }
+
+        private void ShowAndClose()
+        {
+            if (DropDownExpand == "true")
+            {
+                DropDownOpenCss = "dropdown";
+                DropDownExpand = "false";
+            }
+            else
+            {
+                DropDownOpenCss = "dropdown open";
+                DropDownExpand = "true";
+            }
+        }
+    }
+}
